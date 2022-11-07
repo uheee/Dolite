5dd33cd73a414ac39d0bb383a7409080e4a3709d
Nemo.Tao <nemo.tao@refinedchina.com>
2022-11-07 14:31:29 +0800

    feat: add KeyManager & TokenManager into BaseBoard

--- Dolite/Utils/BaseBoard.cs
+++ Dolite/Utils/BaseBoard.cs
@@ -5,5 +5,7 @@ namespace Dolite.Utils;
 public abstract class BaseBoard
 {
     public IMapper Mapper { get; init; } = null!;
+    public KeyManager KeyManager { get; init; } = null!;
+    public TokenManager TokenManager { get; init; } = null!;
     public Resource Resource { get; init; } = null!;
 }
