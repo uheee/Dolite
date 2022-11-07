75f2715a192b91b18b07fbb559ab9fed3a3e1757
Nemo.Tao <nemo.tao@refinedchina.com>
2022-11-07 11:02:49 +0800

    feat: add Token Generate method with JwtPayload

--- Dolite/Utils/TokenManager.cs
+++ Dolite/Utils/TokenManager.cs
@@ -6,6 +6,15 @@ namespace Dolite.Utils;
 
 public static class TokenManager
 {
+    public static string Generate(string keyName, JwtPayload payload)
+    {
+        var privateKey = KeyManager.Private(keyName);
+        var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.EcdsaSha256);
+        var descriptor = new JwtSecurityToken(new JwtHeader(credentials), payload);
+
+        return new JwtSecurityTokenHandler().WriteToken(descriptor);
+    }
+    
     public static string Generate(string keyName, TimeSpan expiration, params Claim[] claims)
     {
         var privateKey = KeyManager.Private(keyName);
