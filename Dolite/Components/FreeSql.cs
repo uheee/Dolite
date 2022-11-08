fddf4e090ac76198aa88b6d7b766340093ded84d
Nemo.Tao <nemo.tao@refinedchina.com>
2022-11-08 10:00:58 +0800

    feat: add FreeSQL builder config parameter

--- Dolite/Components/FreeSql.cs
+++ Dolite/Components/FreeSql.cs
@@ -11,22 +11,26 @@ public class FreeSqlComponent : DoliteComponent
     private readonly Assembly _assembly;
     private readonly bool _autoSync;
     private readonly Action<IFreeSql>? _configAction;
+    private readonly Action<FreeSqlBuilder>? _builderAction;
     private readonly string _connectionString;
 
-    public FreeSqlComponent(Assembly assembly, string connectionString, bool autoSync, Action<IFreeSql>? configAction)
+    public FreeSqlComponent(Assembly assembly, string connectionString, bool autoSync,
+        Action<IFreeSql>? configAction, Action<FreeSqlBuilder>? builderAction)
     {
         _assembly = assembly;
         _connectionString = connectionString;
         _autoSync = autoSync;
         _configAction = configAction;
+        _builderAction = builderAction;
     }
 
     public override void BeforeBuild(WebApplicationBuilder builder)
     {
-        var freeSql = new FreeSqlBuilder()
+        var freeSqlBuilder = new FreeSqlBuilder()
             .UseConnectionString(DataType.PostgreSQL, _connectionString)
-            .UseAutoSyncStructure(_autoSync)
-            .Build();
+            .UseAutoSyncStructure(_autoSync);
+        _builderAction?.Invoke(freeSqlBuilder);
+        var freeSql = freeSqlBuilder.Build();
         _configAction?.Invoke(freeSql);
         builder.Services.AddSingleton(freeSql);
         builder.Services.AddFreeRepository(null, _assembly);
@@ -36,10 +40,11 @@ public class FreeSqlComponent : DoliteComponent
 public static class FreeSqlComponentExtensions
 {
     public static DoliteBuilder UseFreeSql(this DoliteBuilder builder, Assembly assembly,
-        string connectionName = "Default", bool autoSync = false, Action<IFreeSql>? configAction = null)
+        string connectionName = "Default", bool autoSync = false,
+        Action<IFreeSql>? configAction = null, Action<FreeSqlBuilder>? builderAction = null)
     {
         var connectionString = builder.WebAppBuilder.Configuration.GetConnectionString(connectionName);
-        var component = new FreeSqlComponent(assembly, connectionString, autoSync, configAction);
+        var component = new FreeSqlComponent(assembly, connectionString, autoSync, configAction, builderAction);
         return builder.AddComponent(component);
     }
 }
